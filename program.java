3f9cbd1f4509056e1411c75cc164689e08bd7430
Andrei Krylov <dronovoy@yandex.ru>
2023-04-25 22:36:03 +0300

    Third task was done

--- program.java
+++ program.java
@@ -1,11 +1,12 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class program {
     public static void main(String[] args) {
 
-        ex01();
+        ex03();
     }
 
     static List<Integer> ex01() {
@@ -19,7 +20,7 @@ public class program {
         List<Integer> list2 = new LinkedList<Integer>();
 
         Iterator<Integer> listIterator = ((LinkedList<Integer>) list).descendingIterator();
-        while(listIterator.hasNext()){
+        while (listIterator.hasNext()) {
             list2.add(listIterator.next());
         }
 
@@ -28,9 +29,33 @@ public class program {
 
     }
 
+    static int ex03() {
+        // Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке.
+        // Используйте итератор
+        int result = 0;
+        List<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < 11; i++) {
+            list.add(i);
+        }
+        System.out.println("Initial list: " + list);
+
+        ListIterator<Integer> listIterator = list.listIterator();
+
+        while (listIterator.hasNext()) {
+            result += listIterator.next();
+
+        }
+
+        System.out.println("Summ of all elements is: " + result);
+        return result;
+
+    }
 
 
 
 
 
+
+
+    
 }
